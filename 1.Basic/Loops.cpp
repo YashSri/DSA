fd7330819788da32994b72b0065bc077fb38bd95
YashSri <156013191+YashSri@users.noreply.github.com>
2024-06-17 01:21:00 +0530

    Add files via upload

---
+++ 1.Basic/Loops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int a;
+    cout << "enter the number:" << endl;
+    cin >> a;
+
+    if (n > 0)
+    {
+        cout << ("positive") << endl;
+    }
+
+    else
+    {
+        cout << "negative" << endl;
+    }
+
+    return 0;
+}
